0e35f8f31e27df1ca2d6a482a2da58162c085fa6
Ismail Niazi <ismailniazi065@gmail.com>
2025-07-29 06:50:20 -0700

    MainScreen changes

--- MainScreen.js
+++ MainScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity,View,Text ,Image,color} from 'react-native';
-import { useState } from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  color,
+} from 'react-native';
+import {useState} from 'react';
 import HomeScreen from './HomeScreen';
 import BlogScreen from './BlogScreen';
 import MapScreen from './MapScreen';
 
 const MainScreen = () => {
-    const [activeScreen, setActiveScreen] = useState('Home');
-    const renderScreen = () => {
+  const [activeScreen, setActiveScreen] = useState('HomeScreen');
+  const renderScreen = () => {
     switch (activeScreen) {
       case 'HomeScreen':
         return <HomeScreen />;
@@ -20,80 +27,103 @@ const MainScreen = () => {
     }
   };
   return (
-
-    
-    <View style={{flex:1}}>
-          <View style={{ flex: 1 }}>{renderScreen()}</View>
-        <View style={{padding:10,width:'100%',position:'absolute',bottom:10}}>
-             <View  style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center',
-                backgroundColor:'rgb(255, 255, 255)',paddingVertical:10,borderRadius:20,
-                borderColor:'rgb(222, 220, 220)',borderWidth:2}}>
-            <View>
-                <TouchableOpacity onPress={() => setActiveScreen('BlogScreen')}>
-                     <Image
-    source={{ uri: 'asset:/images/job.png' }}
-    style={[
-      styles.icon,
-      activeScreen === 'BlogScreen' && styles.activeIcon
-    ]}
-  />
-  <Text style={activeScreen === 'BlogScreen' ? styles.activeText : styles.tabText}>Blog</Text>
-            </TouchableOpacity> 
-            </View>
-             <View >
-                <TouchableOpacity onPress={() => setActiveScreen('HomeScreen')} >
-                      <Image
-    source={{ uri: 'asset:/images/home.png' }}
-    style={[
-      styles.icon,
-      activeScreen === 'HomeScreen' && styles.activeIcon
-    ]}
-  />
-  <Text style={activeScreen === 'HomeScreen' ? styles.activeText : styles.tabText}>Home</Text>
-            </TouchableOpacity> 
-            </View>
-              <View>
-                <TouchableOpacity onPress={() => setActiveScreen('MapScreen')}>
-                      <Image
-    source={{ uri: 'asset:/images/Map.png' }}
-    style={[
-      styles.icon,
-      activeScreen === 'MapScreen' && styles.activeIcon
-    ]}
-  />
-  <Text style={activeScreen === 'MapScreen' ? styles.activeText : styles.tabText}>Map</Text>
-            </TouchableOpacity> 
-            </View>
-             </View>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>{renderScreen()}</View>
+      <View
+        style={{padding: 10, width: '100%', position: 'absolute', bottom: 10}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            backgroundColor: 'rgb(255, 255, 255)',
+            paddingVertical: 10,
+            borderRadius: 20,
+            borderColor: 'rgb(222, 220, 220)',
+            borderWidth: 2,
+          }}>
+          <View>
+            <TouchableOpacity onPress={() => setActiveScreen('BlogScreen')}>
+              <Image
+                source={{uri: 'asset:/images/job.png'}}
+                style={[
+                  styles.icon,
+                  activeScreen === 'BlogScreen' && styles.activeIcon,
+                ]}
+              />
+              <Text
+                style={
+                  activeScreen === 'BlogScreen'
+                    ? styles.activeText
+                    : styles.tabText
+                }>
+                Blog
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity onPress={() => setActiveScreen('HomeScreen')}>
+              <Image
+                source={{uri: 'asset:/images/home.png'}}
+                style={[
+                  styles.icon,
+                  activeScreen === 'HomeScreen' && styles.activeIcon,
+                ]}
+              />
+              <Text
+                style={
+                  activeScreen === 'HomeScreen'
+                    ? styles.activeText
+                    : styles.tabText
+                }>
+                Home
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity onPress={() => setActiveScreen('MapScreen')}>
+              <Image
+                source={{uri: 'asset:/images/Map.png'}}
+                style={[
+                  styles.icon,
+                  activeScreen === 'MapScreen' && styles.activeIcon,
+                ]}
+              />
+              <Text
+                style={
+                  activeScreen === 'MapScreen'
+                    ? styles.activeText
+                    : styles.tabText
+                }>
+                Map
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
+      </View>
     </View>
-  )
-}
-export default MainScreen
+  );
+};
+export default MainScreen;
 
 const styles = StyleSheet.create({
-
-     icon: {
+  icon: {
     width: 30,
     height: 30,
     tintColor: 'black', // default tint
-    alignSelf:'center'
+    alignSelf: 'center',
   },
   activeIcon: {
     tintColor: '#007AFF', // active tab color
-    alignSelf:'center'
+    alignSelf: 'center',
   },
   tabText: {
     color: '#888',
     fontSize: 16,
-   
   },
   activeText: {
     color: '#007AFF',
     fontWeight: 'bold',
     fontSize: 16,
-    
   },
-  
-
-})
+});
